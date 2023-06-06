33a476af9c17da36d28229d34b803a6592485492
warri <warriorlogan957@gmail.com>
2023-06-06 04:30:12 +0300

    commut

---
+++ vt/MySQL_task3/MySQL_windows_form_task3/MySQL_windows_form_task3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySQL_windows_form_task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost,3306;Database=dager;Uid=root;Pwd=balasgun;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM цех";
+                MySqlCommand command = new MySqlCommand(query, connection);
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+
+                dataAdapter.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при подключении к базе данных: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
